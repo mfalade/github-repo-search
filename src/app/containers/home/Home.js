ff379d5554c6bd6e0f7703083358d778ca3469d9
Mayowa Falade <falademayowa240@gmail.com>
2020-05-16 21:15:50 +0100

    [fix]: show load when fetching route scripts

--- src/app/containers/home/Home.js
+++ src/app/containers/home/Home.js
@@ -84,7 +84,7 @@ function Home() {
         repositories={repositories.items}
       />
       <Pagination
-        visible={totalItemsCount > LIST_ITEMS_PER_PAGE}
+        visible={isFetchComplete && totalItemsCount > LIST_ITEMS_PER_PAGE}
         currentPage={currentPage}
         totalItemsCount={totalItemsCount}
         onPageSelect={handlePageSelect}
